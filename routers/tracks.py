bc0d3a4b00b0bb83594c81fcaa54d14725eab1d9
bzamanbd <riponzamanbd@gmail.com>
2024-05-10 20:30:19 +0600

    Router Setup

--- routers/tracks.py
+++ routers/tracks.py
@@ -5,5 +5,5 @@ track_router = APIRouter()
 tracks:list[str] = ["TrackOne","TrackTwo","TrackThree","TrackFour","TrackFive"]
 
 @track_router.get("/tracks", tags=["Tracks"] )
-def get_tracks()->list[str]:
+async def get_tracks()->list[str]:
     return tracks
